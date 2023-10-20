e0f4e4ed17a2fe4536a01621eed7e90b3df0b663
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-10-20 11:41:30 +0300

    add: `translate` app

---
+++ translate/apps/processor.py
@@ -0,0 +1,67 @@
+import concurrent.futures
+
+import pandas as pd
+from googletrans import Translator
+from malevich.square import DF, processor, scheme
+from pydantic import BaseModel
+
+
+@scheme()
+class TextWithLanguageTranslationPairs(BaseModel):
+    text: str
+    from_language: str
+    to_language: str
+
+
+@processor()
+def translate_texts(
+    text_with_lang_pairs: DF['TextWithLanguageTranslationPairs'],
+):
+    """Translate texts from one language to another
+
+    Input:
+        A dataframe with the following columns:
+            - text: text to translate
+            - from_language: language code to translate from
+            - to_language: language code to translate to
+
+    Configuration:
+        The app is not configurable
+
+    Output:
+        A dataframe with the following columns:
+            - text: original text
+            - from_language: language code to translate from
+            - to_language: language code to translate to
+            - translation: translated text in the target language
+
+    Details:
+        The app utilizes Google Translate API to translate texts.
+
+        Columns `text`, `from_language`, and `to_language` are copied
+        from the input dataframe without any modification.
+
+    Args:
+        docs (DF[TextWithLanguageTranslationPairs]):
+            Input dataframe with texts to translate
+
+    Returns:
+        Dataframe with a column `translation` containing translated texts
+        attached to the end of the input dataframe
+    """
+
+    translator = Translator()
+    def process_row(row: pd.Series) -> str:
+        return translator.translate(
+            row['text'],
+            src=row['from_language'],
+            dest=row['to_language']
+        ).text
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        translations = list(
+            executor.map(process_row, text_with_lang_pairs.to_dict(orient='records')))
+
+    output_df = text_with_lang_pairs.copy()
+    output_df.insert(len(text_with_lang_pairs.columns), 'translation', translations)
+    return output_df
