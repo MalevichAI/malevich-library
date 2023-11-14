038829e4c785008e1353df5f5833b4bf540a2692
RamPrin <l.zelenskiy@innopolis.university>
2023-11-14 18:55:01 +0300

    Added linkedin spiders

--- scrape/apps/spiders/__init__.py
+++ scrape/apps/spiders/__init__.py
@@ -1,5 +1,8 @@
 from .text import TextSpider
-
+from .linked_in_profiles import LinkedInPeopleSpider
+from .linked_in_org import LinkedInCompanySpider
 SPIDERS = {
     'text': TextSpider,
+    'linkedin_people_profile': LinkedInPeopleSpider,
+    'linkedin_company_profile': LinkedInCompanySpider
 }
