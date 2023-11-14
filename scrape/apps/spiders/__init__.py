7ef2b621b3bea6b179d999aaf78f8ba580cefa71
RamPrin <l.zelenskiy@innopolis.university>
2023-11-14 18:55:57 +0300

    Added linkedin org and personal spiders

--- scrape/apps/spiders/__init__.py
+++ scrape/apps/spiders/__init__.py
@@ -1,6 +1,7 @@
 from .text import TextSpider
 from .linked_in_profiles import LinkedInPeopleSpider
 from .linked_in_org import LinkedInCompanySpider
+
 SPIDERS = {
     'text': TextSpider,
     'linkedin_people_profile': LinkedInPeopleSpider,
