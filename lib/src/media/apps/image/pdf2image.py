4b52e761e3c0b2b2a11a53089c2b5ced19cecb9d
RamPrin <pak55256@gmail.com>
2023-12-01 14:42:10 +0300

    add: media pdf2image pages

--- lib/src/media/apps/image/pdf2image.py
+++ lib/src/media/apps/image/pdf2image.py
@@ -13,11 +13,17 @@ class Filename(BaseModel):
 @processor()
 def convert_pdf_to_jpeg(files: DF[Filename], context: Context):
     outputs = []
+    start_page = context.app_cfg.get('start_page', 0)
+    page_num = context.app_cfg.get('page_num', None)
     for filename in files.filename.to_list():
         pages = []
-        images = convert_from_path(context.get_share_path(filename))
+        images = convert_from_path(context.get_share_path(filename),
+                                   first_page=start_page+1,
+                                   last_page=page_num if page_num is None \
+                                    else start_page+page_num)
         for i, image in enumerate(images):
-            result_path = os.path.basename(filename.replace('.pdf', f'_{i}.jpg'))
+            result_path = os.path.basename(filename.replace('.pdf',
+                                                            f'_{start_page+i}.jpg'))
             image.save(
                     os.path.join(
                         APP_DIR,
