1f46c86a920b55a58de13f9a68c7543cadceb55a
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2023-12-06 12:00:53 +0300

    add: pdf2image, upd: slack docs, drives folder download

---
+++ lib/src/media/apps/image/pdf2image.py
@@ -0,0 +1,71 @@
+import os
+
+import pandas as pd
+from malevich.square import APP_DIR, DF, Context, processor, scheme
+from pdf2image import convert_from_path
+from pydantic import BaseModel
+
+
+@scheme()
+class Filename(BaseModel):
+    filename:str
+
+@processor()
+def convert_pdf_to_jpeg(files: DF[Filename], context: Context):
+    """Converts PDF to JPEG
+
+    Inputs:
+        A dataframe with a column `filename` that contains
+        names of the files. Files should be either downloaded
+        or shared in apps before
+
+    Outputs:
+        A dataframe with two columns: `filename` and `jpeg`.
+        The `filename` column contains the name of the original
+        file, the `jpeg` column contains the name of the converted
+        file. The converted file is shared in apps.
+
+    Configuration:
+        - start_page (int): the number of the first page to convert.
+            If not specified, converts from the first page
+        - page_num (int): the number of pages to convert. If not
+            specified, converts all pages
+
+    Args:
+        files (DF[Filename]): a dataframe with a column `filename`
+            that contains names of the files
+        context (Context): a context object that contains
+            the configuration and the methods to work with files
+
+    Returns:
+        DF[Filename]: a dataframe with two columns: `filename` and `jpeg`.
+            The `filename` column contains the name of the original
+            file, the `jpeg` column contains the name of the converted
+            file. The converted file is shared in apps
+
+    """
+    outputs = []
+    start_page = context.app_cfg.get('start_page', 0)
+    page_num = context.app_cfg.get('page_num', None)
+    for filename in files.filename.to_list():
+        pages = []
+        images = convert_from_path(context.get_share_path(filename),
+                                   first_page=start_page+1,
+                                   last_page=page_num if page_num is None \
+                                    else start_page+page_num)
+        for i, image in enumerate(images):
+            result_path = os.path.basename(filename.replace('.pdf',
+                                                            f'_{start_page+i}.jpg'))
+            image.save(
+                    os.path.join(
+                        APP_DIR,
+                        result_path
+                    )
+            )
+            context.share(result_path)
+            pages.append(result_path)
+        df = pd.DataFrame(pages, columns=['jpeg'])
+        df.insert(1, 'filename', filename)
+        outputs.append(df)
+
+    return pd.concat(outputs)
--- lib/src/slack/apps/slack.py
+++ lib/src/slack/apps/slack.py
@@ -10,6 +10,37 @@ class SlackInput(BaseModel):
 
 @processor()
 def send_message(df: DF[SlackInput], context: Context):
+    """Sends message to specified Slack channel
+
+    Inputs:
+
+        A dataframe with a column `channel_id` that contains
+        ids of the channels and a column `message` that contains
+        messages to send. Each row is a separate message
+
+    Outputs:
+
+        A dataframe with two columns: `channel_id` and `message`.
+        The `channel_id` column contains the id of the channel
+        and the `message` column contains the message that was sent.
+
+    Configuration:
+
+        - token (str): Slack token
+
+    Args:
+
+        df (DF[SlackInput]): a dataframe with a column `channel_id`
+            that contains ids of the channels and a column `message`
+            that contains messages to send. Each row is a separate message
+        context (Context): a context object that contains
+            the configuration and the methods to work with files
+
+    Returns:
+        DF[SlackInput]: a dataframe with two columns: `channel_id` and `message`.
+            The `channel_id` column contains the id of the channel
+            and the `message` column contains the message that was sent.
+    """
     token = context.app_cfg.get('token')
     client = WebClient(token=token)
     for _, row in df.iterrows():
