6d77142baed295bab4b2befc2295ba013cfe0c04
RamPrin <pak55256@gmail.com>
2023-12-19 18:48:43 +0300

    aliexpress gets images"

--- lib/src/scrape/apps/spiders/aliexpress.py
+++ lib/src/scrape/apps/spiders/aliexpress.py
@@ -18,6 +18,7 @@ class AliexpSpider(scrapy.Spider):
             start_urls,
             scrape_api_key,
             output_type='json',
+            only_images=False,
             *args,
             **kwargs
         ) -> None:
@@ -25,6 +26,7 @@ class AliexpSpider(scrapy.Spider):
         self.start_urls=start_urls
         self.type = output_type
         self.api_key=scrape_api_key
+        self.img_only=only_images
 
     def form_request(self, url):
         payload = {
@@ -43,21 +45,31 @@ class AliexpSpider(scrapy.Spider):
     def parse(self, response: scrapy.http.Response):
         outputs = json.loads(response.text)
         sel = scrapy.Selector(Response(outputs['result']['content'], response.url))
-        data = {} if self.type == 'json' else ''
-        title = ' '.join(sel.xpath('//h1/text()').getall())
-        description = ' '.join(
-            sel.xpath(
-                "//span[contains(@id, 'tl_')]/text()"
-            ).getall()
-        )
-        characteristics = description + ' ' + ' '.join(sel.xpath(
-            "//span[contains(@class, 'titleContent') or \
-                contains(@class, 'value')]/text()"
-        ).getall())
-        if self.type == 'json':
-            data['title'] = title
-            data['description'] = characteristics
-            yield {'text': json.dumps(data, ensure_ascii=False)}
+        if not self.img_only:
+            data = {} if self.type == 'json' else ''
+            title = ' '.join(sel.xpath('//h1/text()').getall())
+            description = ' '.join(
+                sel.xpath(
+                    "//span[contains(@id, 'tl_')]/text()"
+                ).getall()
+            )
+            characteristics = description + ' ' + ' '.join(sel.xpath(
+                "//span[contains(@class, 'titleContent') or \
+                    contains(@class, 'value')]/text()"
+            ).getall())
+            images = sel.xpath("//div[@class = 'gallery_Gallery__picList__1gsooe']//picture//img/@src").getall()   # noqa: E501
+            if self.type == 'json':
+                data['title'] = title
+                data['description'] = characteristics
+                data['images'] = images
+                yield {'text': json.dumps(data, ensure_ascii=False)}
+            else:
+                data += f'Title:\n{title}\n\nDescription:\n{characteristics}\n\n' + \
+                    'Images:\n'.join(images)
+                yield {'text': data }
         else:
-            data += f'Title:\n{title}\n\nDescription:\n{characteristics}'
-            yield {'text': data }
+            images = sel.xpath(
+                "//div[@class = 'gallery_Gallery__picList__1gsooe']//picture//img/@src"
+            ).getall()
+            for img in images:
+                yield {'text': img}
