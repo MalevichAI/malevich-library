46ff3eb6da1111edf4a1acf1a401cd8b8ace13be
RamPrin <pak55256@gmail.com>
2024-02-03 18:36:39 +0300

    cvat uses cvat_sdk (finished)

--- lib/src/cvat/apps/cvat.py
+++ lib/src/cvat/apps/cvat.py
@@ -1,11 +1,7 @@
-import json
-
-import boto3
 import pandas as pd
-import requests
+from cvat_sdk.api_client import ApiClient, Configuration, models
 from malevich.square import DF, Context, processor, scheme
 from pydantic import BaseModel
-from requests.auth import HTTPBasicAuth
 
 
 @scheme()
@@ -35,15 +31,8 @@ def upload_images_to_task(df: DF[TaskImages], context: Context):
 
         -  `cvat_url` - URL of your CVAT server
 
-        -   `aws_access_key_id`, `aws_secret_access_key` - AWS user credentials
+        -  `project_id` - ID of CVAT project
 
-        -   `endpoint_url` - In case if you do not use default AWS S3, provide endpoint URL
-
-        -   `bucker_name` - Bucket name
-
-        -   `cloud_storage_id` - ID of CVAT cloud storage
-
-        -   `project_id` - ID of CVAT project
 
         Output:
             DF [UploadResult]:
@@ -66,87 +55,49 @@ def upload_images_to_task(df: DF[TaskImages], context: Context):
     cvat_url = context.app_cfg.get('cvat_url', None)
     assert cvat_url, "CVAT link was not provided"
 
-    key_id = context.app_cfg.get('aws_access_key_id', None)
-    secret_key = context.app_cfg.get('aws_secret_access_key', None)
-    assert key_id and secret_key, "AWS credentials were not provided"
-
-    endpoint_url = context.app_cfg.get('endpoint_url', None)
-    bucket_name = context.app_cfg.get('bucket_name', 'cvat')
-
     project_id = context.app_cfg.get('project_id', None)
     assert project_id, "Project ID was not provided"
 
-    cloud_storage_id = context.app_cfg.get('cloud_storage_id', None)
-    assert cloud_storage_id, "Cloud storage ID was not provided"
-
-    auth = HTTPBasicAuth(login, password)
-    client = boto3.client(
-            's3',
-            aws_access_key_id=key_id,
-            aws_secret_access_key=secret_key,
-            endpoint_url=endpoint_url
+    cfg = Configuration(
+        host=cvat_url,
+        username=login,
+        password=password
     )
 
-    results = {}
-    data = requests.get(
-        f'{cvat_url}/api/tasks',
-        auth=auth,
-        json={'project_id': project_id}
-    )
-    data =  json.loads(data.text)
-
-    for d in data['results']:
-        results[d['name']] = d['id']
-
-    while data['next'] is not None:
-        data = requests.get(
-            data['next'],
-            auth=auth,
-            json={'project_id': project_id}
-        )
-        data =  json.loads(data.text)
-
-        for d in data['results']:
-            results[d['name']] = d['id']
-    outputs = []
-    task_names = df['task'].unique().tolist()
-    for task_name in task_names:
-        if task_name not in results.keys():
-            task_id = requests.post(
-                f'{cvat_url}/api/tasks',
-                auth=auth,
-                json={
-                    'name': task_name,
-                    'project_id': project_id
-                }
+    with ApiClient(cfg) as client:
+        outputs = []
+        task_names = df['task'].unique().tolist()
+        for task_name in task_names:
+            task_spec = models.TaskWriteRequest(
+                name=task_name,
+                project_id=project_id
             )
-            task_id = json.loads(task_id.text)
-            results[task_name] = task_id['id']
-        images = df[df['task'] == task_name]['image'].to_list()
+            task, _ = client.tasks_api.create(task_spec)
+            images = df[df['task'] == task_name]['image'].to_list()
 
-        image_set = []
-        for image in images:
-            path = context.get_share_path(image)
-            client.upload_file(path, bucket_name, f'{task_name}/{image}')
-            image_set.append(f'{task_name}/{image}')
+            image_set = []
+            for image in images:
+                path = context.get_share_path(image)
+                image_set.append(open(path, 'rb'))
 
-        status = requests.post(
-            f'{cvat_url}/api/tasks/{results[task_name]}/data',
-            auth=auth,
-            json={
-                'image_quality': 100,
-                'cloud_storage_id': cloud_storage_id,
-                'server_files' : image_set,
-                'storage': 'local',
-                'storage_method': 'file_system'
-            }
+            data = models.DataRequest(
+                image_quality=100,
+                client_files=image_set,
+                use_zip_chunks=True,
+                use_cache=True
+            )
+
+            (_, response) = client.tasks_api.create_data(
+                task.id,
+                data_request=data,
+                _content_type="multipart/form-data"
+            )
+            for image in images:
+                outputs.append([task_name, image, response.status])
+
+        result = pd.DataFrame(
+            outputs,
+            columns=['task', 'image', 'status']
         )
-        for image in image_set:
-            outputs.append([task_name, image, status.status_code])
-
-    result = pd.DataFrame(
-        outputs,
-        columns=['task', 'image', 'status']
-    )
 
     return result
