debed675932d8d12038100301d44429bcf4764bc
RamPrin <pak55256@gmail.com>
2024-02-07 18:27:20 +0300

    add: async image upload

--- lib/src/cvat/apps/cvat.py
+++ lib/src/cvat/apps/cvat.py
@@ -1,3 +1,7 @@
+import multiprocessing as mp
+from concurrent.futures import ProcessPoolExecutor
+
+import boto3
 import pandas as pd
 from cvat_sdk.api_client import ApiClient, Configuration, models
 from malevich.square import DF, Context, processor, scheme
@@ -15,6 +19,24 @@ class UploadResult(BaseModel):
     image: str
     status: str
 
+def upload_to_s3(
+        key_id,
+        secret_key,
+        path,
+        bucket_name,
+        bucket_path,
+        endpoint_url=None
+    ):
+
+    client = boto3.client(
+        's3',
+        aws_access_key_id = key_id,
+        aws_secret_access_key = secret_key,
+        endpoint_url = endpoint_url
+    )
+
+    client.upload_file(path, bucket_name, bucket_path)
+
 @processor()
 def upload_images_to_task(df: DF[TaskImages], context: Context):
     """
@@ -29,9 +51,19 @@ def upload_images_to_task(df: DF[TaskImages], context: Context):
         Configuration:
         -  `cvat_user`, `cvat_password` - CVAT account credentials
 
+        - `cvat_org` - CVAT organization. By default uses personal workspace
+
         -  `cvat_url` - URL of your CVAT server
 
         -  `project_id` - ID of CVAT project
+
+        - `cloud_id` - ID of cloud storage
+
+        - `aws_access_key_id`, `aws_secret_access_key` - AWS credentials
+
+        - `endpoint_url` - If you use another cloud, specify endpoint_url
+
+        - `bucket_name` - Name of S3 bucket
 
 
         Output:
@@ -50,6 +82,7 @@ def upload_images_to_task(df: DF[TaskImages], context: Context):
     """  # noqa: E501
     login = context.app_cfg.get('cvat_user', None)
     password = context.app_cfg.get('cvat_password', None)
+    org = context.app_cfg.get('cvat_org', "")
     assert login and password, "CVAT credentials were not provided"
 
     cvat_url = context.app_cfg.get('cvat_url', None)
@@ -57,6 +90,18 @@ def upload_images_to_task(df: DF[TaskImages], context: Context):
 
     project_id = context.app_cfg.get('project_id', None)
     assert project_id, "Project ID was not provided"
+
+    cloud_id = context.app_cfg.get('cloud_id', None)
+    assert cloud_id, "Cloud ID was not provided"
+
+    aws_key_id = context.app_cfg.get('aws_access_key_id', None)
+    aws_secret_key = context.app_cfg.get('aws_secret_access_key', None)
+    assert aws_key_id and aws_secret_key, "AWS credentials were not provided"
+
+    endpoint_url = context.app_cfg.get('endpoint_url', None)
+
+    bucket_name = context.app_cfg.get('bucket_name', 'cvat')
+
 
     cfg = Configuration(
         host=cvat_url,
@@ -68,29 +113,41 @@ def upload_images_to_task(df: DF[TaskImages], context: Context):
         outputs = []
         task_names = df['task'].unique().tolist()
         for task_name in task_names:
+            executor = ProcessPoolExecutor(mp.cpu_count()//2)
             task_spec = models.TaskWriteRequest(
                 name=task_name,
                 project_id=project_id
             )
-            task, _ = client.tasks_api.create(task_spec)
+            task, _ = client.tasks_api.create(task_spec, org=org)
             images = df[df['task'] == task_name]['image'].to_list()
 
             image_set = []
             for image in images:
                 path = context.get_share_path(image)
-                image_set.append(open(path, 'rb'))
+                executor.submit(
+                    upload_to_s3,
+                    key_id = aws_key_id,
+                    secret_key = aws_secret_key,
+                    path = path,
+                    bucket_name = bucket_name,
+                    bucket_path = f"{task_name}/{image}",
+                    endpoint_url=endpoint_url
+                )
+                image_set.append(f"{task_name}/{image}")
+
+            executor.shutdown(wait=True)
 
             data = models.DataRequest(
                 image_quality=100,
-                client_files=image_set,
+                server_files=image_set,
                 use_zip_chunks=True,
-                use_cache=True
+                use_cache=True,
+                cloud_storage_id=cloud_id
             )
 
             (_, response) = client.tasks_api.create_data(
                 task.id,
-                data_request=data,
-                _content_type="multipart/form-data"
+                data_request=data
             )
             for image in images:
                 outputs.append([task_name, image, response.status])
