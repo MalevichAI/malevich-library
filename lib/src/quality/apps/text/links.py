19701ba6cb3d686dd2185351230b761e0c6ac5b9
RamPrin <pak55256@gmail.com>
2024-02-08 18:19:07 +0300

    fixed PR comments

--- lib/src/quality/apps/text/links.py
+++ lib/src/quality/apps/text/links.py
@@ -9,27 +9,42 @@ class Links(BaseModel):
     link: str
 
 @processor()
-def asser_links(df: DF[Links], context: Context):
+def asser_links(df: DF[Links], context: Context) -> pd.DataFrame:
     """
         Check if the links are valid (do not return error status codes)
 
-        Input:
-          DF[Links] - DataFrame with one column named 'link'.
+    ## Input:
+           A dataframe that contains columns:
+                - link: link to be validated
 
-        Output:
-          DF[Links] - DataFrame with one column named 'link' containing
-          valid or invalid links depending on 'filter_links' option.
+    ## Output:
+            The format of dataframe depends on the configuration provided.
 
-        Options:
-          filter_links - If True, returns links which passed assert. Else return
-          error links.
-    """
+            If `filter_links` set to True, the output will be a dataframe
+            with a column named `link` containing valid links.
+
+            Otherwise the output will be a dataframe with a column named
+            `error` containing invalid links.
+
+    ## Configuration:
+        - filter_links(bool, optional): If set to True, will filter the dataframe and exclude invalid links. Default value is False
+
+    ---
+
+    Args:
+        df (DF[Links]): A dataframe with a column named `link` containing links.
+
+    Returns:
+        If `filter_links` is true, returns a dataframe with a column named `links` containing valid links.
+        Otherwise, returns a dataframe with a column named `error` containing invalid links
+
+    """  # noqa: E501
     errors = []
     valids = []
     for link in df['link'].to_list():
         try:
-            status = requests.get(link)
-            if status.status_code < 400:
+            response = requests.get(link)
+            if response.status_code < 400:
                 valids.append(link)
             else:
                 errors.append(link)
@@ -37,7 +52,7 @@ def asser_links(df: DF[Links], context: Context):
             errors.append(link)
 
     if not context.app_cfg.get('filter_links', False):
-        return pd.DataFrame(errors, columns=['link'])
+        return pd.DataFrame(errors, columns=['errors'])
 
     else:
         return pd.DataFrame(valids, columns=['link'])
