cde228a8b8ed42892976c998ed0a3a20e4abe920
Aleksandr Lobanov <aleksandr.lobanov.official@gmail.com>
2024-02-22 14:40:02 +0300

    add: hugging face apps, aliexpress reworked, squash_columns, cvat updated, audiocraft

---
+++ lib/src/huggingface/apps/pipelines/text/conversational.py
@@ -0,0 +1,138 @@
+import uuid
+
+import pandas as pd
+import pydantic
+import torch
+from malevich.square import DF, Context, processor, scheme
+from transformers import Conversation, pipeline
+
+
+@scheme()
+class DialogMessageInput:
+    content: str
+    """The content of the message"""
+    role: str
+    """An identifier of the role of the message. It can be "user" or "assistant"."""
+    # dialog_id: Optional[str] = None
+
+
+class ConversationalPipelineConfig(pydantic.BaseModel):
+    model: str = "facebook/blenderbot-400M-distill"
+    """Name of the model to use in the pipeline"""
+
+    min_length_for_response: int = 32
+    """(int, optional, defaults to 32) — The minimum length (in number of tokens) for a response."""  # noqa: E501
+
+    minimum_tokens: int = 10
+    """(int, optional, defaults to 10) — The minimum length of tokens to leave for a response."""  # noqa: E501
+
+
+@processor()
+def continue_conversation(message: DF[DialogMessageInput], context: Context):
+    """
+    Starts or continues a conversation with the given messages using HuggingFace Transformers.
+
+    ## Input:
+
+        A dataframe with columns:
+
+        - `content` (string): The content of the message
+        - `role` (string):
+            An identifier of the role of the message.
+            It can be "user" or "assistant".
+        - `dialog_id` (string, optional):
+            An identifier of the dialog.
+            If not provided, a new dialog will be created.
+
+    ## Output:
+
+        Output contains all the input messages and the responses from the model.
+        It is a dataframe with columns:
+
+        - `dialog_id` (string): An identifier of the dialog
+        - `content` (string): The content of the message
+        - `role` (string): An identifier of the role of the message
+
+    ## Configuration:
+
+        - `model`: string, default "facebook/blenderbot-400M-distill".
+            Name of the model to use in the pipeline
+        - `min_length_for_response`: int, default 32.
+            The minimum length (in number of tokens) for a response.
+        - `minimum_tokens`: int, default 10.
+            The minimum length of tokens to leave for a response.
+
+    -----
+
+    Args:
+        message: input collection with messages
+        context (dict): configuration (see above)
+
+    Returns:
+        Collection with messages and responses
+    """  # noqa: E501
+    try:
+        config = ConversationalPipelineConfig(**context.app_cfg)
+    except pydantic.ValidationError as err: 
+        context.logger.error(
+            "Got an error while trying to get the config. "
+            '. '.join([
+                f"Field `{err['loc'][0]}` is not correct: {err['msg']}"
+                for err in err.errors()
+            ])
+        )
+        raise
+
+    p = pipeline(
+        # https://huggingface.co/docs/transformers/v4.37.2/en/main_classes/pipelines#transformers.Conversation
+        model=config.model,
+        task='conversational',
+        device='cuda' if torch.cuda.is_available() else 'cpu',
+    )
+
+
+    if 'dialog_id' not in message.columns:
+        message.insert(0, 'dialog_id', [
+            uuid.uuid4() for _ in range(len(message.index))
+        ])
+
+    message[message.dialog_id.isna()].dialog_id = [
+        uuid.uuid4() for _ in range(len(message[message.dialog_id.isna()].index))
+    ]
+
+    conversations = []
+    for dialog_id, group in message.groupby('dialog_id'):
+        messages = group[['content', 'role']].to_dict(orient='records')
+        conversations.append(Conversation(messages, dialog_id))
+
+    context.logger.info(f"Got {len(conversations)} conversations")
+    responses: list[Conversation] = p(conversations)
+
+    if not isinstance(responses, list):
+        # If the pipeline returns a single conversation,
+        # we need to wrap it into a list
+        responses = [responses]
+    context.logger.info(f"Got {len(responses)} responses from model")
+
+    output_records = []
+    for conversation in responses:
+        try:
+            context.logger.info(
+                f"Conversation: {conversation.uuid}, "
+                f"n messages: {len(conversation.messages)}"
+            )
+            for message in conversation.messages:
+                output_records.append({
+                    'dialog_id': str(conversation.uuid),
+                    'content': message['content'],
+                    'role': message['role']
+                })
+        except Exception:
+            context.logger.error(
+                "Encountered an error while processing the response. "
+                f"type(conversation): {type(conversation) if conversation else None}. "
+                f"dir(conversation): {dir(conversation) if conversation else None}. "
+            )
+            raise
+
+    return pd.DataFrame(output_records)
---
+++ lib/src/huggingface/apps/pipelines/text/question_answering.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import pydantic
+import torch
+from malevich.square import DF, Context, processor, scheme
+from transformers import Conversation, pipeline
+
+
+@scheme()
+class QuestionInput:
+    question: str
+    """The content of the message"""
+    context: str
+    """A context for the question"""
+
+class ConversationalPipelineConfig(pydantic.BaseModel):
+    model: str = "deepset/roberta-base-squad2"
+    """Name of the model to use in the pipeline"""
+
+
+@processor()
+def answer_questions(questions: DF[QuestionInput], context: Context):
+    """
+    Answers questions using HuggingFace Transformers.
+
+    ## Input:
+
+        A dataframe with columns:
+
+        - `question` (string): The content of the question
+        - `context` (string): A context for the question
+
+    ## Output:
+
+        It is a dataframe with columns:
+
+            - score (float): The probability associated to the answer.
+            - start (integer):
+                The character start index of the answer
+                (in the tokenized version of the input).
+            - end (integer):
+                The character end index of the answer
+                (in the tokenized version of the input).
+            - answer (string): The answer to the question.
+
+    ## Configuration:
+
+        - `model`: string. default "deepset/roberta-base-squad2".
+            Name of the model to use in the pipeline
+
+    -----
+
+    Args:
+
+       questions: A collection of questions and contexts
+       config: Configuration (see above)
+
+    Returns:
+        Collection with answers, scores and indices of the answers
+    """
+    try:
+        config = ConversationalPipelineConfig(**context.app_cfg)
+    except pydantic.ValidationError as err:
+        context.logger.error(
+            "Got an error while trying to get the config. "
+            '. '.join([
+                f"Field `{err['loc'][0]}` is not correct: {err['msg']}"
+                for err in err.errors()
+            ])
+        )
+        raise
+
+    p = pipeline(
+        model=config.model,
+        task='question-answering',
+        device='cuda' if torch.cuda.is_available() else 'cpu',
+    )
+
+    responses: list[Conversation] = p(questions.to_dict(orient='records'))
+    if not isinstance(responses, list):
+        # If the pipeline returns a single conversation,
+        # we need to wrap it into a list
+        responses = [responses]
+
+    return pd.DataFrame(responses)
